5512575b0b0890064211197959c166dac76495b5
Ming Guan <guanminggege@gmail.com>
2024-10-29 17:11:52 +1300

    Remove aws_profile default

--- ec2/c1-versions.tf
+++ ec2/c1-versions.tf
@@ -7,16 +7,15 @@ terraform {
       version = ">= 5.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0"
-    }        
+    }
   }
 }
 
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "default"
   profile = var.aws_profile
 }
 /*
